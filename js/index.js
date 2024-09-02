4e2d88e4a291ee1c8e2184c4d9976ac8bdd68058
kimbboyong <kimbboyong@gmail.com>
2024-09-02 11:41:57 +0900

    feat: section add

--- js/index.js
+++ js/index.js
@@ -10,6 +10,17 @@ var swiper = new Swiper(".section04 .mySwiper", {
     },
   },
 });
+var swiper = new Swiper(".sectionNews .mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 2000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
 handleScrollTop();
 
